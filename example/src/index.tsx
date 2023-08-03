5249879159d6dc4bd6004b236aea9fc6fe3ed9f4
sandygudie <goodnewssandy@gmail.com>
2023-08-03 19:04:58 +0100

    review description

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,7 +5,7 @@ import { CountryFlag, CountryCode, CountryName } from 'react-countryname-flag'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <CountryFlag countryName={'Afghanistan'} style={{ height: '10px' }} />
+    <CountryFlag countryName={'Afghanistan'} style={{ fontSize: '30px' }} />
     <br />
     <CountryFlag countryCode={'AG'} />
     <br />
